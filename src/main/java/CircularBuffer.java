6fedef7d87075f85e2f0e6c849f6258fed1c37df
Ratthawan Sermsuwannasuk <ratthawanser@mathregit.group>
2020-08-03 17:08:41 +0700

    replace when array full

---
+++ src/main/java/CircularBuffer.java
@@ -0,0 +1,33 @@
+public class CircularBuffer {
+
+    private int bufferSize = 10;
+    //string array name buffer
+    private String[] buffer;
+    private int readPointer;
+    private int writePointer;
+
+    //Default constructure
+    public CircularBuffer() {
+        this.buffer = new String [10];
+    }
+
+    public boolean isEmpty() {
+        //buffer should have 10 space
+        return bufferSize == 10;
+    }
+
+    public void writeData(String input) {
+        //receive input then decrease space
+        bufferSize--;
+        this.buffer[writePointer++] = input;
+    }
+
+    public boolean isFull() {
+        //check from bufferSize if =0 so now it's full
+        return bufferSize == 0;
+    }
+
+    public String readData() {
+        return this.buffer[readPointer++];
+    }
+}
---
+++ src/test/java/CircularBufferTest.java
@@ -0,0 +1,42 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CircularBufferTest {
+    @Test
+    public void create_new_buffer_should_empty(){
+        CircularBuffer cb = new CircularBuffer();
+        boolean result = cb.isEmpty();
+        assertTrue("Buffer is NOT available",result);
+    }
+
+    @Test
+    public void create_new_buffer_with_default_size_should_10() {
+        CircularBuffer cb = new CircularBuffer();
+        for(int i=0; i<10; i++) {
+            cb.writeData("A"+i);
+        }
+        boolean result = cb.isFull();
+        assertTrue("Buffer is NOT FULL",result);
+    }
+
+    @Test
+    public void write_A_B_to_buffer_should_read_A_B_from_buffer(){
+        CircularBuffer cb = new CircularBuffer();
+        cb.writeData("A");
+        cb.writeData("B");
+        assertEquals("A",cb.readData());
+        assertEquals("B",cb.readData());
+    }
+
+    @Test
+    public void replace_A_to_B_when_buffer_is_full(){
+        CircularBuffer cb = new CircularBuffer();
+        for(int i=0; i<10; i++) {
+            cb.writeData("A"+i);
+            cb.readData();
+        }
+        cb.writeData("B");
+        assertEquals("B", cb.readData());
+    }
+}
